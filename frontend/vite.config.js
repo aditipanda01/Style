25b2a899e409eb7431b9980fe67d3c1a6dcbc965
Aditi Panda <pandaaditi37@gmail.com>
2025-10-26 19:51:19 +0530

    modified vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,11 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: {
-       '/api': 'https://style-bcgu.onrender.com',
-    },
+    proxy: process.env.NODE_ENV === 'development' ? {
+      '/api': 'http://localhost:5000', // only for local dev
+    } : undefined,
   },
 });
